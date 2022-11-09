a4317059595c46fbca30ead1efeac3b8e5ef8649
brenobcsilva <breno.csilva@hotmail.com>
2022-11-09 15:53:51 -0300

    Criado classes de seguranca

---
+++ Admin/src/main/java/com/ecommerce/admin/admin/config/AdminConfiguration.java
@@ -0,0 +1,29 @@
+package com.ecommerce.admin.admin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class AdminConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new AdminServiceConfig();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService());
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+}
--- Admin/src/main/java/com/ecommerce/admin/admin/config/AdminServiceConfig.java
+++ Admin/src/main/java/com/ecommerce/admin/admin/config/AdminServiceConfig.java
@@ -3,10 +3,13 @@ package com.ecommerce.admin.admin.config;
 import com.ecommerce.library.library.model.Admin;
 import com.ecommerce.library.library.repository.AdminRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.stream.Collectors;
 
 public class AdminServiceConfig implements UserDetailsService {
 
@@ -19,6 +22,8 @@ public class AdminServiceConfig implements UserDetailsService {
         if(admin == null){
             throw new UsernameNotFoundException("Could not find username");
         }
-        return new User(admin.getUsername(), );
+        return new User(admin.getUsername(), admin.getPassword(), admin
+                .getRoles().stream().map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList()));
     }
 }
